566a7b73ba7fb91d0e8df59c28e2101f38f8de67
kangyouknowwho <848744804@qq.com>
2020-09-23 10:21:07 +0800

    sjdfalskdf

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@
 // import Vue from 'vue'
 // import Router from 'vue-router'
 
-import Home from '../components/Home.vue'
 import Content from '../components/Content.vue'
 
 // export default new Router({ // ⬅本地嵌入时
@@ -11,7 +10,7 @@ export default new VueRouter({ // ⬅cdn方式引入时
         {
             path: '/',
             name: 'home', 
-            component: Home,
+            component: Content,
         }
     ],
 });
